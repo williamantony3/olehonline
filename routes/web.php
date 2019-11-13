b1cb22b93878b36e78250d5a653db9ca9e4abb07
William Antony <williamantony@ymail.com>
2019-11-13 22:23:09 +0700

    perbaiki web.php

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,13 @@ Route::get('/checkout', 'TransactionHeader@checkout');
 Route::post('/transactions/add', 'TransactionHeader@add');
 Route::get('/transactions/addDetails/{id}', 'TransactionDetail@add');
 Route::get('/transactions/view', 'TransactionHeader@view');
-Route::get('/transactions/confirm/{id}', 'TransactionHeader@confirm');
-Route::get('/transactions/accept/{id}', 'TransactionHeader@accept');
-Route::get('/transactions/deny/{id}', 'TransactionHeader@deny');
 Route::get('/product/type/{id}', 'Product@viewByType');
 Route::get('/product/from/{id}', 'Product@viewByProvince');
 Route::get('/explore', 'Province@view');
 Route::get('/province/detail/{id}', 'Province@detail');
 Route::get('/contact', 'Index@contact');
 Route::get('/product/search/', 'Product@search');
+if(Session::get('status')==1){
 Route::get('/product/view/', 'Product@view');
 Route::get('/product/edit/{id}', 'Product@edit');
 Route::get('/product/add', 'Product@add');
@@ -44,3 +42,7 @@ Route::get('/product/edit/{id}', 'Product@edit');
 Route::post('/product/editProductPost/{id}', 'Product@editProductPost');
 Route::get('/product/changePictureForm/{id}', 'Product@changePictureForm');
 Route::post('/product/changePic/{id}', 'Product@changePic');
+Route::get('/transactions/confirm/{id}', 'TransactionHeader@confirm');
+Route::get('/transactions/accept/{id}', 'TransactionHeader@accept');
+Route::get('/transactions/deny/{id}', 'TransactionHeader@deny');
+}
